c2fa7786877e63654d737824745137256530a751
kundalik jadhav <kundalikjadhav0807@gmail.com>
2025-03-02 20:24:27 +0530

    refactor: simplify metadata handling for About Us page

--- app/(admin)/about-us/page.jsx
+++ app/(admin)/about-us/page.jsx
@@ -1,16 +1,14 @@
 import React from "react";
-import Head from "next/head";
+
+export const metadata = {
+  title: "About us",
+  description:
+    "QA Testing Playground is a hub for automation testers to practice and enhance their skills. Join our community to explore, contribute, and collaborate.",
+};
 
 const AboutPage = () => {
   return (
     <>
-      <Head>
-        <title>About Us - QA Testing Playground</title>
-        <meta
-          name="description"
-          content="QA Testing Playground is a hub for automation testers to practice and enhance their skills. Join our community to explore, contribute, and collaborate on automation testing projects."
-        />
-      </Head>
       <div className="min-h-screen container mx-auto max-w-5xl text-foreground bg-background pt-5">
         <h1 className="text-3xl font-bold mb-4">About Us</h1>
         <blockquote className="italic">
@@ -22,9 +20,7 @@ const AboutPage = () => {
             A hub for automation testers seeking hands-on practice, project
             ideas, and collaboration opportunities. Our platform categorizes
             projects across various domains, making it easy to find the right
-            challenge for your skill level. Whether you're a beginner or an
-            experienced tester, you can explore, contribute, and collaborate on
-            automation testing projects.
+            challenge for your skill level.
           </p>
           <p className="mb-4">
             Join our <strong>community guide</strong>, where contributors can
