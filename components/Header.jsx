b3b9e761b20275a8143b2b82f31b742a1b7ce71c
kundalik jadhav <kundalikjadhav0807@gmail.com>
2025-03-11 10:34:09 +0530

    feat: update favicon and logo assets; remove unused icons and add new logo

--- components/Header.jsx
+++ components/Header.jsx
@@ -1,19 +1,11 @@
 "use client";
 
-import { Bot, ChevronDown, Menu } from "lucide-react";
+import { CirclePlay, Code, Menu } from "lucide-react";
 import Link from "next/link";
 import React from "react";
-import {
-  DropdownMenu,
-  DropdownMenuContent,
-  DropdownMenuItem,
-  DropdownMenuTrigger,
-} from "./ui/dropdown-menu";
 import { Button } from "./ui/button";
 import SheetOpen from "./NavbarSheet";
 import { ModeToggle } from "./lib/Mode-toggle";
-import { basicDetails } from "@/data/BasicSetting";
-import Image from "next/image";
 
 const Header = () => {
   const mainNavLinks = [
@@ -24,16 +16,14 @@ const Header = () => {
   ];
 
   return (
-    <header className="fixed top-0 w-full bg-white/80 dark:bg-black backdrop-blur-md z-50 border-b shadow-sm">
+    <header className="fixed top-0 w-full bg-white/80 dark:bg-black backdrop-blur-md z-50 border-b shadow-sm py-2">
       <nav className="container mx-auto px-1  flex items-center justify-between">
         <Link href="/" passHref>
-          <Image
-            src="/logo/QALogo.webp"
-            alt="QA PlayGround Logo"
-            width={250}
-            height={100}
-            title="QA PlayGround Logo"
-          />
+          <span className="text-2xl flex items-center gap-2">
+            QA Play
+            <CirclePlay />
+            Ground
+          </span>
         </Link>
         {/* Mobile Menu */}
         <div className="block md:hidden">
